4b55dd8c33a492c73c5824c410e5b4dd15893aca
zAllan007 <abreuallandantas@gmail.com>
2026-06-16 15:35:55 -0300

    feat: Adds the section knowing the france

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const elements = document.querySelectorAll(".hidden");
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+elements.forEach((el) => observer.observe(el));
